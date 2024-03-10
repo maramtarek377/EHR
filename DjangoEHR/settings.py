eb3a9fc77211a91bb058c9ceda84508eccfe964b
maramtarek377 <14712020100068@stud.cu.edu.eg>
2024-03-10 16:44:56 +0200

    data migration

--- DjangoEHR/settings.py
+++ DjangoEHR/settings.py
@@ -78,25 +78,25 @@ WSGI_APPLICATION = 'DjangoEHR.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 # version 1
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-   }
-
-# # version2
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'lab3database',
-#         'PASSWORD': 'Mypassword3',
-#         'HOST': 'ehrserver.postgres.database.azure.com',
-#         'PORT': '5432',
-#         'OPTIONS':{'sslmode':'require'},
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
-# }
+#    }
+
+# # version2
+DATABASES = {
+  'default': {
+   'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'maram123',
+        'USER': 'lab3database',
+        'PASSWORD': 'M/12/33M',
+        'HOST': 'Burstable, B1ms, 1 vCores, 2 GiB RAM, 32 GiB storage',
+        'PORT': '5432',
+        'OPTIONS':{'sslmode':'require'},
+    }
+}
 
 
 # Password validation
